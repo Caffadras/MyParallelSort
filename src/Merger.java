f3a25d505d82a2818d6e9a7ffa32c5c7a2512442
Caffadras <senya.dubik@gmail.com>
2022-03-05 18:56:48 +0200

    init

---
+++ src/Merger.java
@@ -0,0 +1,20 @@
+
+public class Merger {
+	private int[] mainArray;
+	private int offset;
+	private int from; 
+	private int to;
+	
+	
+	public Merger(int[] array, int offset, int from, int to) {
+		mainArray = array;
+		this.offset = offset; 
+		this.from = from;
+		this.to  = from;
+	}
+	
+	
+	public static void merge(int[] destinationArray, int[] leftArray, int[] rightArray) {
+		
+	}
+}
